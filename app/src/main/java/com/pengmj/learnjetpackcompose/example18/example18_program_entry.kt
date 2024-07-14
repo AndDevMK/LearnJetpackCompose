b18b214220cd0204beafe2261c85b1166451cfa3
pengmingjian <pengmingjian@kingpointcn.com>
2024-07-14 08:17:09 +0800

    手势动画之滑动删除

---
+++ app/src/main/java/com/pengmj/learnjetpackcompose/example18/example18_program_entry.kt
@@ -0,0 +1,135 @@
+package com.pengmj.learnjetpackcompose.example18
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.calculateTargetValue
+import androidx.compose.animation.splineBasedDecay
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.horizontalDrag
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.util.VelocityTracker
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
+
+/**
+ * @author MinKin
+ * @date 2024/7/13
+ * @desc
+ */
+
+@Composable
+fun Example18ProgramEntry(
+    modifier: Modifier = Modifier,
+) {
+
+    var list by remember {
+        mutableStateOf((1 until 50).toMutableList())
+    }
+
+    LazyColumn(
+            modifier = modifier,
+    ) {
+        items(items = list, key = { it }) {
+            SwipeItem(index = it) {
+                list = list.toMutableList().apply { remove(it) }
+            }
+        }
+    }
+}
+
+@Composable
+private fun SwipeItem(
+    index: Int,
+    onDismissed: () -> Unit,
+) {
+    Box(
+            modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .swipeToDismiss {
+                        onDismissed()
+                    },
+    ) {
+        Text(
+                text = "Item $index",
+                modifier = Modifier.align(Alignment.Center),
+        )
+    }
+}
+
+private fun Modifier.swipeToDismiss(
+    onDismissed: () -> Unit
+): Modifier = composed {
+    val offsetX = remember { Animatable(0f) }
+    pointerInput(Unit) {
+        // Used to calculate fling decay.
+        val decay = splineBasedDecay<Float>(this)
+        // Use suspend functions for touch events and the Animatable.
+        coroutineScope {
+            while (true) {
+                val velocityTracker = VelocityTracker()
+                // Stop any ongoing animation.
+                offsetX.stop()
+                awaitPointerEventScope {
+                    // Detect a touch down event.
+                    val pointerId = awaitFirstDown().id
+
+                    horizontalDrag(pointerId) { change ->
+                        // Update the animation value with touch events.
+                        launch {
+                            offsetX.snapTo(
+                                    offsetX.value + change.positionChange().x
+                            )
+                        }
+                        // 记录滑动位置
+                        velocityTracker.addPosition(
+                                change.uptimeMillis, change.position
+                        )
+                    }
+                }
+                // No longer receiving touch events. Prepare the animation.
+                val velocity = velocityTracker.calculateVelocity().x
+                // 计算投掷速度
+                val targetOffsetX = decay.calculateTargetValue(
+                        offsetX.value, velocity
+                )
+                // The animation stops when it reaches the bounds.
+                offsetX.updateBounds(
+                        lowerBound = -size.width.toFloat(), upperBound = size.width.toFloat()
+                )
+                launch {
+                    if (targetOffsetX.absoluteValue <= size.width) {
+                        // Not enough velocity; Slide back.
+                        offsetX.animateTo(
+                                targetValue = 0f, initialVelocity = velocity
+                        )
+                    }
+                    else {
+                        // The element was swiped away.
+                        offsetX.animateDecay(velocity, decay)
+                        onDismissed()
+                    }
+                }
+            }
+        }
+    }.offset { IntOffset(offsetX.value.roundToInt(), 0) }
+}
